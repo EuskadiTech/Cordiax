61eed7adbb55d8012133e11cab04607b98afd530
Naiel <109038805+naielv@users.noreply.github.com>
2026-01-29 11:22:34 +0100

    fix

--- cordiax.py
+++ cordiax.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 import webview
 import json
-config = json.decode(open("_config.json", "r"))
+config = json.load(open("_config.json", "r"))
 window = webview.create_window('Axia4 - EntreAulas', 'https://tech.eus/entreaulas/')
 webview.start(user_agent=config["ua"])
