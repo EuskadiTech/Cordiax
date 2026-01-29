073c4644dc2f7516c70a7227f9f1006a2f159a3e
Naiel <109038805+naielv@users.noreply.github.com>
2026-01-29 11:18:57 +0100

    add config

--- cordiax.py
+++ cordiax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import webview
-
+import json
+config = json.decode(open("_config.json", "r"))
 window = webview.create_window('Axia4 - EntreAulas', 'https://tech.eus/entreaulas/')
-webview.start()
+webview.start(user_agent=config["ua"])
